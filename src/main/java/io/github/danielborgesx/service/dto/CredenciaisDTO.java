54652a9cbd054ced9c3ab429c0fa71a554c3c60c
Daniel Borges <danieel.bogees@gmail.com>
2023-06-26 15:50:34 -0300

    Implementação do JWT token

---
+++ src/main/java/io/github/danielborgesx/service/dto/CredenciaisDTO.java
@@ -0,0 +1,13 @@
+package io.github.danielborgesx.service.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+
+    private String login;
+    private String senha;
+
+}
